78bc48ab22017c12387f2114deeca8c7bf084861
Chiemi Watanabe <chiemi@slis.tsukuba.ac.jp>
2019-08-22 23:14:46 +0900

    BASIC認証をかけた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    before_action :basic
+
+    private
+
+    def basic
+      authenticate_or_request_with_http_basic do |name, password|
+        name == ENV['PYWT_USER'] && password == ENV['PYWT_PASS']
+      end
+    end
 end
