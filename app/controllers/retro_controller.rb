7f4d49d7fc9fa44bc625b35da27414069b6adc72
Chiemi Watanabe <chiemi@slis.tsukuba.ac.jp>
2019-08-23 14:24:54 +0900

    人・チーム・日で表示

--- app/controllers/retro_controller.rb
+++ app/controllers/retro_controller.rb
@@ -1,11 +1,12 @@
 class RetroController < ApplicationController
+
   def showall
     @parameters = {team: selected_team(params[:team_id]), date: selected_date(params[:date])}
     @retrospectives = Retrospective.select_retro(params[:team_id],params[:date])
     @team_items = Team.all.collect{|t| [t.name,t.id]}.unshift(["全てのチーム","all"])
     @date_items = Retrospective.select(:date).distinct.collect{|d| [d.date]}.unshift(["全ての日","all"])
 
-    @selected_dates = @retrospectives.collect{|r| r.date }.uniq
+    @dates = @retrospectives.collect{|r| r.date }.uniq
 
   end
 
@@ -17,6 +18,28 @@ class RetroController < ApplicationController
     end
     @retrospectives = Retrospective.day_retro(@day)
     @teams = @retrospectives.collect{|r| r.member.team_id }.uniq
+  end
+
+  def member
+    if params[:member_id] && Member.exists?(params[:member_id]) then
+      @member = Member.find(params[:member_id])
+      @retrospectives = Retrospective.where(member_id: @member)
+      @dates = @retrospectives.collect{|r| r.date }.uniq
+    else
+      flash[:danger] = "The member is not found."
+      redirect_to :root
+    end    
+  end
+
+  def team
+    if params[:team_id] && Team.exists?(params[:team_id]) then
+      @team = Team.find(params[:team_id])
+      @retrospectives =  Retrospective.joins(:member).where(members: {team_id: @team}).order(:date, :member_id)
+      @dates = @retrospectives.collect{|r| r.date }.uniq    
+    else
+      flash[:danger] = "The team is not found."
+      redirect_to :root
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'admin/load'
   post 'admin/load'
   get 'retro/showall'
-  get 'retro/:date', to: 'retro#day'
+  get 'd/:date', to: 'retro#day'
+  get 'm/:member_id', to: 'retro#member'
+  get 't/:team_id', to: 'retro#team'
   root 'retro#showall'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
