6ecdab33244b98214d36c69ef3ed5395293c880b
Chiemi Watanabe <chiemi@a.tsukuba-tech.ac.jp>
2019-09-25 16:09:55 +0900

    typo修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'personalywt.herokuapp.com'
   config.action_mailer.default_url_options = {host: host}
-  ActionMailer::Base.smtp_settions = {
+  ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
